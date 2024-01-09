a592ecd43e4012cc89f2b80ec73776949801d38c
jaao-web <joaovgarciaa@gmail.com>
2024-01-08 21:33:23 -0300

    segundo commit

---
+++ src/main/java/com/joaodev/meutodo/MeutodoApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.meutodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeutodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeutodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaodev/meutodo/MeutodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.meutodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeutodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
